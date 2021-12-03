94726bcfdd725f348c46a06b8668d943266b99f9
hsattar <39243545+hsattar@users.noreply.github.com>
2021-12-03 10:38:05 +0000

    can read and post reviews

--- src/lib/fs-tools.js
+++ src/lib/fs-tools.js
@@ -1,1 +1,11 @@
 import fs from 'fs-extra'
+import { fileURLToPath } from 'url'
+import { dirname, join } from 'path'
+
+const {readJSON, writeJSON } = fs
+
+const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), '../data')
+const reviewsJSONPath = join(dataFolderPath, '/reviews.json')
+
+export const getReviews = () => readJSON(reviewsJSONPath)
+export const writeReview = content => writeJSON(reviewsJSONPath, content)
--- src/reviews/reviews.js
+++ src/reviews/reviews.js
@@ -1,18 +1,30 @@
 import express from 'express'
+import { getReviews, writeReview } from '../lib/fs-tools.js'
+import uniqid from 'uniqid'
 
 const reviewsRouter = express.Router()
 
 reviewsRouter.route('/')
 .get(async (req, res, next) => {
     try {
-        res.send('OK')    
+        const reviews = await getReviews()
+        res.send(reviews)    
     } catch (error) {
         next(error)
     }
 })
 .post(async (req, res, next) => {
     try {
-        res.send('OK')
+        const reviews = await getReviews()
+        const newReview = {
+            id: uniqid(),
+            ...req.body,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        }
+        reviews.push(newReview)
+        await writeReview(reviews)
+        res.status(201).send(newReview)    
     } catch (error) {
         next(error)
     }
@@ -21,14 +33,16 @@ reviewsRouter.route('/')
 reviewsRouter.route('/:reviewId')
 .put(async (req, res, next) => {
     try {
-        res.send('OK')
+        const reviews = await getReviews()
+        res.send(reviews)    
     } catch (error) {
         next(error)
     }
 })
 .delete(async (req, res, next) => {
     try {
-        res.send('OK')
+        const reviews = await getReviews()
+        res.send(reviews)    
     } catch (error) {
         next(error)
     }
